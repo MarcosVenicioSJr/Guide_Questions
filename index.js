92c5db6e76ac748bf028160daa493e9598bd8281
Marcos Venicio <marcos.venicio1230@hotmail.com>
2022-08-06 19:36:19 -0300

    Adc bootstrap

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
 app.get("/", (req, res) => {
     res.send("Bem vindo ao meu site")
