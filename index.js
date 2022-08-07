24fea0764ec3de72d1b6a86434a1754643ab0877
Marcos Venicio <marcos.venicio1230@hotmail.com>
2022-08-07 09:32:17 -0300

    Adc o body-parse para receber as perguntas

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
     res.render("index")
