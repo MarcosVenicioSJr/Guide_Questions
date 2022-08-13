f4a38a0b97b8af6e3cbda6920c41857b0d0b71d1
Marcos Venicio <marcos.venicio1230@hotmail.com>
2022-08-13 19:49:23 -0300

    Add respostas na pagina

--- index.js
+++ index.js
@@ -61,4 +61,15 @@ app.get("/pergunta/:id", (req, res) => {
     })
 })
 
+app.post('/responder', (req, res) => {
+    const corpo = req.body.corpo
+    const perguntaId = req.body.pergunta
+    Resposta.create({
+        corpo: corpo,
+        perguntaId: perguntaId
+    }).then(() => {
+        res.redirect('/pergunta/' + perguntaId) // retorna para a página da pergunta respondida.
+    })
+})
+
 app.listen(8000, () => { console.log("Programa rodando!") })
